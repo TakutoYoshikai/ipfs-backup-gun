81c7491a55a2d21a76c7798967f11fcf672bbc43
lucky <takuto.yoshikai@gmail.com>
2021-06-03 19:41:10 +0900

    bin

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const Gun = require("gun");
 const gun = Gun(['https://mvp-gun.herokuapp.com/gun', 'https://e2eec.herokuapp.com/gun']);
+const crypto = require("crypto");
+const fs = require("fs");
+const path = require("path");
 
-function BackupStore (username, password) {
+function BackupStore () {
+  const userHome = process.env[process.platform == "win32" ? "USERPROFILE" : "HOME"];
+  const privKey = fs.readFileSync(path.join(userHome, ".ipfs-backup-key"), "utf8");
+  const username = crypto.createHash("sha256").update(privKey + ":username").digest("hex").slice(0, 24);
+  const password = crypto.createHash("sha256").update(privKey + ":password").digest("hex").slice(0, 24);
   const user = gun.user();
-  const key = "backup_ids";
   this.init = function() {
     return new Promise((resolve, reject) => {
       user.auth(username, password, function(response) {
@@ -20,7 +26,7 @@ function BackupStore (username, password) {
       });
     });
   }
-  this.setBackupId = function(backupId) {
+  this.setBackupId = function(key, backupId) {
     return new Promise((resolve, reject) => {
       user.get(key).put({backupId}, (response) => {
         if (response.err) {
@@ -31,7 +37,7 @@ function BackupStore (username, password) {
       });
     });
   }
-  this.getNewestBackupId = function() {
+  this.getNewestBackupId = function(key) {
     return new Promise((resolve, reject) => {
       let backupId;
       user.get(key).map().once(function(_backupId, id) {
